d305557a18dad42a79094a2e62840e3a1f31ede7
sharmarohit04 <72381613+sharmarohit04@users.noreply.github.com>
2023-05-31 21:27:40 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   build:{
     lib:{
-      entry:'./src/main.js',
+      entry:'index.html',
       name:"login-component"
     }
   },
